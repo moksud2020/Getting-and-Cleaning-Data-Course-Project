496a750a4edd3bee17726c0ea3fbbf79974c734e
moksud2020 <moksud@metrouni.edu.bd>
2020-06-05 11:08:39 +0600

    Data download and unziped sucessfully.

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,10 @@
+library(dplyr)
 
+if(!file.exists("data.zip")){
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileUrl, "data.zip", method = "curl")
+}
+
+if (!file.exists("data")) { 
+  unzip("data.zip")
+}
