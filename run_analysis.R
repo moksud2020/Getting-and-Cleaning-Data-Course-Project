89d3087063332eb28ad104552b27fe7f210eee77
moksud2020 <moksud@metrouni.edu.bd>
2020-06-05 11:56:13 +0600

    Read all data sets from the text files.

--- run_analysis.R
+++ run_analysis.R
@@ -8,3 +8,32 @@ if(!file.exists("data.zip")){
 if (!file.exists("data")) { 
   unzip("data.zip")
 }
+
+# ------READING ALL DATA FROM DIRECTORY--------
+
+
+# read features data
+
+features <- read.table("UCI HAR Dataset/features.txt")
+
+# read activity labels data
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
+# read subject_test.txt
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+# read X_test.txt
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+
+# read y_test.txt
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+
+# read subject_train
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+# read X_train
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+
+# read y_train
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
