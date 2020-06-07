6f90e8bc2d3986ba0cf73a718ca4c046cd4078f9
moksud2020 <moksud@metrouni.edu.bd>
2020-06-07 11:43:28 +0600

    run_analysis.R and CodeBook has done.

--- run_analysis.R
+++ run_analysis.R
@@ -2,15 +2,17 @@ library(dplyr)
 
 # Download data from the url
 
-if(!file.exists("data.zip")){
-  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  download.file(fileUrl, "data.zip", method = "curl")
+fileUrl <-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+fileName <- "UCI HAR Dataset.zip"
+
+if(!file.exists(fileName)){
+  download.file(fileUrl, fileName , method = "curl")
 }
 
 # Unzip data from the downloaded zip file
 
-if (!file.exists("data")) { 
-  unzip("data.zip")
+if (!file.exists(fileName)) { 
+  unzip(fileName)
 }
 
 # ------READING ALL DATA FROM DIRECTORY--------
@@ -54,6 +56,7 @@ y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 colnames(y_train) <- c("id")
 
 # Step 1. Merges the training and the test sets to create one data set. 
+
 mergedDataX <- rbind(X_train, X_test)
 
 mergedDataY <- rbind(y_train, y_test)
@@ -65,7 +68,9 @@ mergedData <- cbind(mergedDataSubject, mergedDataX, mergedDataY)
 # Step 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
 column_names <- make.names(names=names(mergedData), unique=TRUE, allow_ = TRUE)
+
 names(mergedData) <- column_names
+
 masterDataSets <- mergedData %>% select(subject, id, contains("mean"), contains("std"))
 
 # Step 3. Uses descriptive activity names to name the activities in the data set
@@ -75,5 +80,20 @@ masterDataSets$id <- activity_labels[masterDataSets$id, 2]
 
 # Step 4. Appropriately labels the data set with descriptive variable names.
 
+names(masterDataSets)[2] = "activity_name"
+names(masterDataSets)<-gsub("^f", "Frequency", names(masterDataSets))
+names(masterDataSets)<-gsub("^t", "Time", names(masterDataSets))
+names(masterDataSets)<-gsub("Acc", "Accelerometer", names(masterDataSets))
+names(masterDataSets)<-gsub("Gyro", "Gyroscope", names(masterDataSets))
+names(masterDataSets)<-gsub("Mag", "Magnitude", names(masterDataSets))
+names(masterDataSets)<-gsub("Freq()", "Frequency", names(masterDataSets))
+names(masterDataSets)<-gsub("mean()", "Mean", names(masterDataSets))
+names(masterDataSets)<-gsub("std()", "STD", names(masterDataSets))
+names(masterDataSets)<-gsub("BodyBody", "Body", names(masterDataSets))
+
+
 # Step 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+finalDataSets <- masterDataSets %>%group_by(subject, activity_name) %>% summarise_each(funs(mean))
+
+write.table(finalDataSets, "TidyData.txt", row.name=FALSE)
